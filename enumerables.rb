faf7cc762b2eecca351a2ca2ab7058e66f4d3187
Mihndim <jelsieprisca2016@gmail.com>
2021-02-18 14:49:45 -0500

    add my_select method

--- enumerables.rb
+++ enumerables.rb
@@ -13,12 +13,21 @@ def my_each_with_index
   end
 end
 
+def my_select 
+  choice = []
+  length.times do |i|
+    evaluation = yield to_a[i] 
+    choice.push(to_a[i]) if evaluation == true
+  end
+end
 
 puts "This is my-each method"
 
 [1, 2, 3, 4, 5, 6, 7,].my_each { |num| puts num }
 
 [1, 2, 3, 4, 5, 6, 7,].my_each_with_index { |num, i| puts "#{i} : #{num}" }
+
+[1, 2, 3, 4, 5, 6, 7,].my_select { |num| puts num if num.even? }
 end
 
 
