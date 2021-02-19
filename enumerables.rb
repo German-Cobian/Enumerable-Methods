34ce555f3d9a11c799078a6e32d6fd17fb4fa014
Mihndim <jelsieprisca2016@gmail.com>
2021-02-19 16:07:18 -0500

    Fix linter errors

--- enumerables.rb
+++ enumerables.rb
@@ -4,8 +4,8 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
 
-    self.length.times do |i|
-    yield (to_a[i])
+    length.times do |i|
+      yield to_a[i]
     end
     self
   end
@@ -13,24 +13,24 @@ module Enumerable
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
-    self.length.times do |i|
-    yield to_a[i], i
+    length.times do |i|
+      yield to_a[i], i
     end
   end
 
-  def my_select 
+  def my_select
     return to_enum(:my_select) unless block_given?
-    
+
     result = []
-    self.my_each { |i| result << i if yield(i) }
+    my_each { |i| result << i if yield(i) }
     result
   end
 
-  def my_all 
+  def my_all
     return to_enum(:my_all) unless block_given?
 
     result = true
-    self.my_each{ |i| result = false unless yield(i)}
+    my_each { |i| result = false unless yield(i) }
     result
   end
 
@@ -38,15 +38,15 @@ module Enumerable
     return to_enum(:my_any) unless block_given?
 
     result = false
-    self.my_each { |i| result = true if yield(i) }
+    my_each { |i| result = true if yield(i) }
     result
   end
-    
-  def my_none 
+
+  def my_none
     return to_enum(:my_none) unless block_given?
 
     result = true
-    self.my_each { |i| result = false if yield(i) }
+    my_each { |i| result = false if yield(i) }
     result
   end
 
@@ -54,13 +54,13 @@ module Enumerable
     return to_enum(:my_count) unless block_given?
 
     result = 0
-    self.my_each { |i| result += 1 if yield(i) }
+    my_each { |i| result += 1 if yield(i) }
     result
   end
 
   def my_map(my_proc = nil)
     return to_enum(:my_map) unless block_given? || my_proc
-    
+
     arr = []
     if my_proc
       to_a.my_each { |val| arr << my_proc.call(val) }
@@ -89,55 +89,54 @@ module Enumerable
       return 'input error'
     end
     memo
-  end  
+  end
 
   def multiply_els(arr)
     arr.my_inject(:*)
   end
-
 end
 
 # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
-puts "Output for my_each method"
+puts 'Output for my_each method'
 
 test_array = [1, 2, 3, 4, 5, 6, 7, 8]
-arr = [2,4,5]
+arr = [2, 4, 5]
 
 p test_array.my_each { |num| puts num }
 
-puts "Output for my_each_with_index method"
+puts 'Output for my_each_with_index method'
 
 p test_array.my_each_with_index { |num, i| puts "#{i} : #{num}" }
 
-puts "Output for my_select method"
+puts 'Output for my_select method'
 
 p test_array.my_select { |num| puts num if num.even? }
 
-puts "Output for my_all method"
+puts 'Output for my_all method'
 
-p test_array.my_all { |num| num%1 == 0 }
+p test_array.my_all { |num| num % 1 == 0 }
 
-puts "Output for my_any method"
+puts 'Output for my_any method'
 
-p test_array.my_any { |num| num%9 == 0 }
+p test_array.my_any { |num| num % 9 == 0 }
 
-puts "Output for my_none method"
+puts 'Output for my_none method'
 
-p test_array.my_none { |num| num%8 == 0 }
+p test_array.my_none { |num| num % 8 == 0 }
 
-puts "Output for my_count method"
+puts 'Output for my_count method'
 
 p test_array.my_count { |num| puts num }
 
-puts "Output for my_map method"
+puts 'Output for my_map method'
 
 p test_array.my_map { |num| num * 2 }
 
-puts "Output for my_inject method"
+puts 'Output for my_inject method'
 
 p test_array.my_inject { |sum, i| sum + i }
 
-puts "Ouput of my inject test array"
+puts 'Ouput of my inject test array'
 
 p arr.multiply_els(arr)
