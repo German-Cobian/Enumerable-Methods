6f0abbe1902bf0bc4875f206f98e765cb3b16f6d
German Renato Cobian <germancobian@hotmail.com>
2021-03-10 07:53:14 -0800

    Create test for my_each method when a block is passed

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -7,8 +7,15 @@ describe 'Enumerables' do
   let(:num_array2) { [7,5,3,5,2] }
 
   describe '#my_each' do
-    it "returns an enumerator if no block is given " do
-      expect(array.my_each).to be_an(Enumerator)
+
+    it "works identically to ruby\'s #each method when a block is given" do
+    expect(num_array.my_each { |elem| (elem * 5)}).to eq(num_array.each { |i| i.send('*', 5)})
     end
+
+    it "returns an enumerator if no block is given " do
+      expect(num_array.my_each).to be_an(Enumerator)
+    end
+
+    
   end
 end
